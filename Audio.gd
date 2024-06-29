2ee953682afad1b8e7b748203305132fb46b4c8d
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2024-06-28 21:09:57 -0600

    BG and more visualizers, better BPM system

--- Audio.gd
+++ Audio.gd
@@ -4,16 +4,16 @@ var beat = 0
 var beatOdd = 0
 var bpm = 143
 
+
 var vol = 0
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	var path = OS.get_executable_path().get_base_dir() + "/song.ogg"
 	$CustomPlayer.stream = AudioStreamOggVorbis.load_from_file(path)
 	$CustomPlayer.play()
 	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	vol = AudioServer.get_bus_peak_volume_right_db(0,0)
 	beat += float(bpm)/60/60
@@ -22,14 +22,20 @@ func _physics_process(delta):
 		beat = 0
 		print("BEAT")
 		$"Rotating Lights".play("default")
+		$Glow.play("default")
 
 		beatOdd += 1
 		if beatOdd == 2:
 			$Bars2.play("default")
+			$Camera2D.position.x = 920
+			$Camera2D.rotation_degrees = 5
 			beatOdd = 0
 		elif beatOdd == 1:
 			$Bars.play("default")
+			$Camera2D.position.x = 1000
+			$Camera2D.rotation_degrees = -5
 		
+
 	$Visualizer3.value = vol
 	$Visualizer10.value = vol
 	
